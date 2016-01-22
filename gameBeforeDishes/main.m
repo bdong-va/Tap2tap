ba198a92854fec5b77566a39aef015dd99deb5c8
bdong <bdong@vendasta.com>
2016-01-22 14:41:16 -0600

    Initial Commit

---
+++ gameBeforeDishes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gameBeforeDishes
+//
+//  Created by bdong on 2016-01-22.
+//  Copyright © 2016 bdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
