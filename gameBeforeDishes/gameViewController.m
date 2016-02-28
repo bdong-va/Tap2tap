4fb18757342a06d8661e26e00e58ae93cd027399
bdong <bdong@vendasta.com>
2016-02-28 11:52:51 -0600

    add button colour change code.

---
+++ gameBeforeDishes/gameViewController.m
@@ -0,0 +1,60 @@
+//
+//  gameViewController.m
+//  gameBeforeDishes
+//
+//  Created by bdong on 2016-02-28.
+//  Copyright © 2016 bdong. All rights reserved.
+//
+
+#import "gameViewController.h"
+#import "gameConst.h"
+
+UIColor* winColor;
+UIColor* lostColor;
+gameConst* constString;
+@implementation gameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.buttonList = [NSMutableArray arrayWithObjects:_button1,_button2,_button3,_button4, nil];
+    winColor = [UIColor greenColor];
+    lostColor= [UIColor redColor];
+    constString = [[gameConst alloc] init];
+    
+}
+
+-(IBAction)pressPlayerButton:(id)sender
+{
+    [self setButtonsStatus:sender isItWin:true];
+}
+
+
+- (void)setButtonsStatus:(UIButton*)pressedButton isItWin:(BOOL)isGoodTimeToPress
+{
+    if (isGoodTimeToPress) {
+        [pressedButton setBackgroundColor:winColor];
+        [pressedButton setTitle:[constString.PositiveWinText objectAtIndex:arc4random() % [constString.PositiveWinText count]] forState:UIControlStateNormal];
+        for (UIButton *button  in _buttonList) {
+            if (button != pressedButton) {
+                [button setTitle:[constString.NegativeLostText objectAtIndex:arc4random() % [constString.NegativeLostText count]] forState:UIControlStateNormal];
+                [button setBackgroundColor:lostColor];
+            }
+        }
+    }else {
+        [pressedButton setBackgroundColor:lostColor];
+        [pressedButton setTitle:[constString.PositiveLostText objectAtIndex:arc4random() % [constString.PositiveLostText count]] forState:UIControlStateNormal];
+        for (UIButton *button  in _buttonList) {
+            if (button != pressedButton) {
+                [button setTitle:[constString.NegativeWinText objectAtIndex:arc4random() % [constString.NegativeWinText count]] forState:UIControlStateNormal];
+                [button setBackgroundColor:winColor];
+
+            }
+        }
+    }
+    
+}
+
+
+
+@end
